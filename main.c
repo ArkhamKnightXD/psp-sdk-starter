5791d7726c078c8041d6b214c9d616686bd9405c
knight <karvin_jimenez@hotmail.com>
2024-05-03 01:10:27 -0400

    finishing pong.

--- main.c
+++ main.c
@@ -13,65 +13,63 @@ PSP_MAIN_THREAD_ATTR(THREAD_ATTR_VFPU | THREAD_ATTR_USER);
 
 char list[0x20000] __attribute__((aligned(64)));
 
-int yPos = SCREEN_HEIGHT / 2 - 48;
-int yPos2 = SCREEN_HEIGHT / 2 - 48;
-
-int ballXPos = SCREEN_WIDTH / 2 - 16;
-int ballYPos = SCREEN_HEIGHT / 2 - 16;
-int speed = 5; 
-
-void initGu() {
-
+void initGu()
+{
     sceGuInit();
 
-    //Set up buffers
+    // Set up buffers
     sceGuStart(GU_DIRECT, list);
-    sceGuDrawBuffer(GU_PSM_8888,(void*)0,BUFFER_WIDTH);
-    sceGuDispBuffer(SCREEN_WIDTH,SCREEN_HEIGHT,(void*)0x88000,BUFFER_WIDTH);
-    sceGuDepthBuffer((void*)0x110000,BUFFER_WIDTH);
+    sceGuDrawBuffer(GU_PSM_8888, (void *)0, BUFFER_WIDTH);
+    sceGuDispBuffer(SCREEN_WIDTH, SCREEN_HEIGHT, (void *)0x88000, BUFFER_WIDTH);
+    sceGuDepthBuffer((void *)0x110000, BUFFER_WIDTH);
 
-    //Set up viewport
+    // Set up viewport
     sceGuOffset(2048 - (SCREEN_WIDTH / 2), 2048 - (SCREEN_HEIGHT / 2));
     sceGuViewport(2048, 2048, SCREEN_WIDTH, SCREEN_HEIGHT);
     sceGuEnable(GU_SCISSOR_TEST);
     sceGuScissor(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    //Set some stuff
-    sceGuDepthRange(65535, 0); //Use the full buffer for depth testing - buffer is reversed order
+    // Set some stuff
+    sceGuDepthRange(65535, 0); // Use the full buffer for depth testing - buffer is reversed order
 
-    sceGuDepthFunc(GU_GEQUAL); //Depth buffer is reversed, so GEQUAL instead of LEQUAL
-    sceGuEnable(GU_DEPTH_TEST); //Enable depth testing
+    sceGuDepthFunc(GU_GEQUAL);  // Depth buffer is reversed, so GEQUAL instead of LEQUAL
+    sceGuEnable(GU_DEPTH_TEST); // Enable depth testing
 
     sceGuFinish();
     sceGuDisplay(GU_TRUE);
 }
 
-void endGu(){
+void endGu()
+{
     sceGuDisplay(GU_FALSE);
     sceGuTerm();
 }
 
-void startFrame(){
+void startFrame()
+{
     sceGuStart(GU_DIRECT, list);
     sceGuClearColor(0x00000000); // Black background
     sceGuClear(GU_COLOR_BUFFER_BIT);
 }
 
-void endFrame(){
+void endFrame()
+{
     sceGuFinish();
     sceGuSync(0, 0);
     sceDisplayWaitVblankStart();
     sceGuSwapBuffers();
 }
 
-typedef struct {
+typedef struct
+{
     unsigned short u, v;
     short x, y, z;
 } Vertex;
 
-void drawRect(float x, float y, float w, float h) {
+void drawRect(float x, float y, float w, float h)
+{
 
-    Vertex* vertices = (Vertex*)sceGuGetMemory(2 * sizeof(Vertex));
+    Vertex *vertices = (Vertex *)sceGuGetMemory(2 * sizeof(Vertex));
 
     vertices[0].x = x;
     vertices[0].y = y;
@@ -83,37 +81,75 @@ void drawRect(float x, float y, float w, float h) {
     sceGuDrawArray(GU_SPRITES, GU_TEXTURE_16BIT | GU_VERTEX_16BIT | GU_TRANSFORM_2D, 2, 0, vertices);
 }
 
-int main() {
+typedef struct
+{
+    int x, y;
+    int w, h;
+} Rectangle;
 
+_Bool hasCollision(Rectangle player, Rectangle ball)
+{
+
+    return player.x < ball.x + ball.w && player.x + player.w > ball.x &&
+           player.y < ball.y + ball.h && player.y + player.h > ball.y;
+}
+
+int main()
+{
     initGu();
+
+    Rectangle player1 = {8, SCREEN_HEIGHT / 2 - 48, 8, 48};
+    Rectangle player2 = {SCREEN_WIDTH - 16, SCREEN_HEIGHT / 2 - 48, 8, 48};
+    Rectangle ball = {SCREEN_WIDTH / 2 - 16, SCREEN_HEIGHT / 2 - 16, 16, 16};
+
+    int playerSpeed = 6;
+    int ballVelocityX = 4;
+    int ballVelocityY = 4;
 
     SceCtrlData pad;
 
-    int running = 1;
-
-    while(running){
-
+    while (1)
+    {
         sceCtrlReadBufferPositive(&pad, 1);
-        
-        if (pad.Buttons & PSP_CTRL_UP && yPos > 0)
-            yPos -= speed;
 
-        else if (pad.Buttons & PSP_CTRL_DOWN && yPos < SCREEN_HEIGHT - 48)
-            yPos += speed;
+        if (pad.Buttons & PSP_CTRL_UP && player1.y > 0)
+            player1.y -= playerSpeed;
 
-        else if (pad.Buttons & PSP_CTRL_TRIANGLE && yPos2 > 0)
-            yPos2 -= speed;
+        else if (pad.Buttons & PSP_CTRL_DOWN && player1.y < SCREEN_HEIGHT - 48)
+            player1.y += playerSpeed;
 
-        else if (pad.Buttons & PSP_CTRL_CROSS && yPos2 < SCREEN_HEIGHT - 48)
-            yPos2 += speed;
+        if (pad.Buttons & PSP_CTRL_TRIANGLE && player2.y > 0)
+            player2.y -= playerSpeed;
+
+        else if (pad.Buttons & PSP_CTRL_CROSS && player2.y < SCREEN_HEIGHT - 48)
+            player2.y += playerSpeed;
+
+        if (ball.y < 0 || ball.y > SCREEN_HEIGHT - 16)
+        {
+            ballVelocityY *= -1;
+        }
+
+        if (ball.x > SCREEN_WIDTH + 16 || ball.x < -16)
+        {
+            ball.x = SCREEN_WIDTH / 2 - 16;
+            ball.y = SCREEN_HEIGHT / 2 - 16;
+        }
+
+        if (hasCollision(player1, ball) || hasCollision(player2, ball))
+        {
+            ballVelocityX *= -1;
+        }
+
+        ball.x += ballVelocityX;
+        ball.y += ballVelocityY;
 
         startFrame();
 
-        drawRect(8, yPos, 8, 48); // Use xPos and yPos as the coordinates
+        drawRect(player1.x, player1.y, player1.w, player1.h); // Use xPos and yPos as the coordinates
 
-        drawRect(ballXPos, ballYPos, 16, 16); 
+        drawRect(ball.x, ball.y, ball.w, ball.h);
 
-        drawRect(SCREEN_WIDTH - 16, yPos2, 8, 48); 
+        drawRect(player2.x, player2.y, player2.w, player2.h);
 
         endFrame();
 
