e29ec45ad0356c191544985c401753f5f5c27e82
knight <karvin_jimenez@hotmail.com>
2024-05-11 11:34:10 -0400

    added git ignore.

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
  
 //This 2 lines are necessary to setup psp module info. 
 PSP_MODULE_INFO("gutest", 0, 1, 0);
-PSP_MAIN_THREAD_ATTR(THREAD_ATTR_VFPU | THREAD_ATTR_USER); 
+PSP_MAIN_THREAD_ATTR(THREAD_ATTR_VFPU | THREAD_ATTR_USER);  
 
 typedef struct
 {
